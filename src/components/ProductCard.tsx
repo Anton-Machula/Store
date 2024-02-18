334833f8cabb975620afa6f85d9a0eb2d8899b25
Anton Machula <krava34007@gmail.com>
2024-02-18 20:19:10 +0200

    add new styles and update ProductCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -17,27 +17,59 @@ export const ProductCard: FC<ProductCardProps> = ({ product }) => {
   const { title, description, price, category, image, rating } = product;
 
   return (
-    <Card sx={{ border: '1px solid black', p: 1, maxWidth: '500px' }}>
+    <Card
+      variant="outlined"
+      sx={{
+        p: 1,
+        boxShadow: '0 1px 3px rgba(0, 127, 255, 0.1)',
+        display: 'flex',
+        gap: 1,
+        flexDirection: {
+          xs: 'column',
+          sm: 'row',
+        },
+        maxWidth: '700px',
+      }}
+    >
       <CardMedia
         component="img"
         alt={title}
         image={image}
-        sx={{ maxWidth: '100px' }}
+        sx={{
+          maxWidth: '300px',
+          borderRadius: 0.5,
+        }}
       />
-      <CardContent>
+      <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
         <Typography variant="h5">{title}</Typography>
         <Typography variant="body2" color="text.secondary">
           {description}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Price: ${price}
+        <Typography
+          sx={{ display: 'flex', alignItems: 'center', gap: 1 }}
+          variant="body2"
+          color="text.secondary"
+        >
+          <Typography sx={{ fontWeight: 700 }}>Price:</Typography> ${price}
+        </Typography>
+        <Typography
+          sx={{ display: 'flex', alignItems: 'center', gap: 1 }}
+          variant="body2"
+          color="text.secondary"
+        >
+          <Typography sx={{ fontWeight: 700 }}>Category:</Typography>
+          <Chip label={category} color="primary" />
+        </Typography>
+        <Typography
+          sx={{ display: 'flex', alignItems: 'center', gap: 1 }}
+          variant="body2"
+          color="text.secondary"
+        >
+          <Typography sx={{ fontWeight: 700 }}>Rating:</Typography>
+          <Rating value={rating.rate} precision={0.5} readOnly />
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Category: <Chip label={category} color="primary" />
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Rating: <Rating value={rating.rate} readOnly /> {rating.count}
-          reviews
+          {rating.count} reviews
         </Typography>
       </CardContent>
     </Card>
