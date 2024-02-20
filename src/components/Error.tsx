6b7ff5493d08f2c230585e17a4485bb26203e742
Anton Machula <krava34007@gmail.com>
2024-02-20 20:29:40 +0200

    add AlertTitle to Error

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,8 +1,9 @@
-import { Alert } from '@mui/material';
+import { Alert, AlertTitle } from '@mui/material';
 
 export const Error = () => {
   return (
     <Alert severity="error" sx={{ mt: 3 }}>
+      <AlertTitle>Error</AlertTitle>
       Failed to load products
     </Alert>
   );
